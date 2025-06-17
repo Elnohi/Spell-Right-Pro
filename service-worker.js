3f04d7083474ea4b77c205411b854a3ad6f372e5
Elnohi <samielnohi@yahoo.com>
2025-06-16 20:05:56 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,46 @@
-self.addEventListener('install', function (event) {
+const CACHE_NAME = 'spell-right-pro-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/app.js',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('spell-right-cache').then(function (cache) {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './icon-192.png',
-                './icon-512.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
