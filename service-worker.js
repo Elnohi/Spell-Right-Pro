ee0b106e364e714aec853d5d8d6c57f7a984e72c
Elnohi <samielnohi@yahoo.com>
2025-06-17 11:37:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,14 +9,23 @@ const ASSETS_TO_CACHE = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const asset of ASSETS_TO_CACHE) {
+        try {
+          await cache.add(asset);
+        } catch (err) {
+          console.warn(`[Service Worker] Failed to cache: ${asset}`, err);
+        }
+      }
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
+
